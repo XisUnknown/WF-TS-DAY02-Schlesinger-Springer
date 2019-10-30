f90bd93541ae4fd76399228aed422627d2da78a3
roman.cf <roman.springer@outlook.com>
2019-10-30 14:47:47 +0100

    vehicles

--- advanced.js
+++ advanced.js
@@ -4,7 +4,6 @@
         prodYear = "";
         hp = "";
         tracks = "";
-
         constructor(brand, model, prodYear, hp, tracks) {
             this.brand = brand;
             this.model = model;
@@ -15,29 +14,47 @@
         namE() { 
             return ` The ${this.prodYear}er ${this.model} from ${this.brand} has ${this.hp}hp `;
         }
+        calculate(){
+            if (this.constructor.name == "Motor"){
+                return  this.prodYear * this.hp
+            }else{
+                console.log("error");
+            }
+        }
+    }
+
+    class Trucks extends Vehicles{
+        loading;
+        loadingType;  
+        constructor(brand, model, prodYear, hp, tracks, loading, loadingType){
+            super(brand, model, prodYear, hp, tracks);
+            this.loading;
+            this.loadingType;
+        }
+        work(){
+            return ` `;
+        }
     }
 
     class Motor extends Vehicles{
-        salary;
-        jobLocation;
-
-        constructor(name, age, jobTitle, salary, jobLocation){
-            super(name, age, jobTitle);
-            this.salary;
-            this.jobLocation;
-        }
-
-        work(){
-            return ` ${super.namE()} and I get ${this.salary} every month, and I work in ${this.jobLocation}`;
+        type;
+        constructor(brand, model, prodYear, hp, tracks, type){
+            super(brand, model, prodYear, hp, tracks);
+            this.type;
         }
     }
 
     var vehicle1 = new Vehicles("opel", "kadett", "1976", "80", "2");
+    var vehicle2 = new Motor("Yamaha", "Tenere 700", "2018", "73", "1","Adventure");
+    var vehicle3 = new Trucks("MAN", "gross", "2018", "473", "2","12000","flatbed");
 
 
-    console.log(vehicle1.namE());
+    console.log(vehicle2.constructor.name);
+
 
     $("body").append(`
       <p> ${vehicle1.namE()}</p>
+      <p> ${vehicle2.calculate()}</p>
+      <p> ${vehicle3.calculate()}</p>
     `);
 
